ce18e33428c751c65932509abfbc19b159ef6001
Jack Curvey <curveyja@msu.edu>
2023-10-28 12:29:12 -0400

    Initial Commit

---
+++ Glasses Connect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glasses Connect
+//
+//  Created by Jack Curvey on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glasses Connect/Glasses_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glasses_ConnectApp.swift
+//  Glasses Connect
+//
+//  Created by Jack Curvey on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Glasses_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
